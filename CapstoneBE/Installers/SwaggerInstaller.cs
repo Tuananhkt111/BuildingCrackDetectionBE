b1fb161ee21d425b7669a28793f9bc8e7437f40d
Tuấn Anh Hoàng <tuananhkt111@gmail.com>
2020-12-03 14:57:20 +0700

    add swagger configuration

---
+++ CapstoneBE/Installers/SwaggerInstaller.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CapstoneBE.Installers
+{
+    public class SwaggerInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Capstone API",
+                    Description = "Capstone ASP.NET Core Web API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Tuan Anh Hoang",
+                        Email = "tuananhkt111@gmail.com",
+                        Url = new Uri("https://www.facebook.com/profile.php?id=100008471889226"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under Tau Hai Team"
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {new OpenApiSecurityScheme{Reference = new OpenApiReference
+                    {
+                        Id = "Bearer",
+                        Type = ReferenceType.SecurityScheme
+                    }}, new List<string>()}
+                });
+            });
+        }
+    }
+}
--- CapstoneBE/Startup.cs
+++ CapstoneBE/Startup.cs
@@ -1,19 +1,10 @@
 using AutoMapper;
-using CapstoneBE.Data;
 using CapstoneBE.Installers;
-using CapstoneBE.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CapstoneBE
 {
@@ -37,19 +28,27 @@ namespace CapstoneBE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Swagger configuration
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Capstone API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseHttpsRedirection();
             app.UseRouting();
-            // global cors policy
+            // Global cors policy
             app.UseCors(x => x
                 .AllowAnyOrigin()
                 .AllowAnyMethod()
                 .AllowAnyHeader());
+            //Authentication & authorization
             app.UseAuthentication();
             app.UseAuthorization();
+            //Endpoints
             app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
