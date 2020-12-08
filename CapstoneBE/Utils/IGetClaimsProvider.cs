074ce2fc584fad6aac955f64582b990854d87bef
Tuấn Anh Hoàng <tuananhkt111@gmail.com>
2020-12-08 17:58:11 +0700

    del

--- CapstoneBE/Utils/IGetClaimsProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CapstoneBE.Utils
-{
-    //Extract claims information from JWT
-    public interface IGetClaimsProvider
-    {
-        public string UserId { get; }
-        public string Role { get; }
-    }
-}
