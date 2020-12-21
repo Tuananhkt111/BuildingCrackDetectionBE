4e0e9f409380686e0bb6daae899b2bb400ca1af7
Tuấn Anh Hoàng <tuananhkt111@gmail.com>
2020-12-21 17:39:31 +0700

    update crackinfo

--- CapstoneBE/Models/Custom/Cracks/CrackInfo.cs
+++ CapstoneBE/Models/Custom/Cracks/CrackInfo.cs
@@ -7,12 +7,16 @@ namespace CapstoneBE.Models.Custom.Cracks
         public int CrackId { get; set; }
         public int? MaintenanceOrderId { get; set; }
         public int LocationId { get; set; }
+        public string LocationName { get; set; }
         public string ReporterId { get; set; }
+        public string ReporterName { get; set; }
         public string Position { get; set; }
         public string Description { get; set; }
         public string Severity { get; set; }
         public string Status { get; set; }
         public string Image { get; set; }
+        public int AssessmentResult { get; set; }
+        public string AssessmentDescription { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastModified { get; set; }
     }
