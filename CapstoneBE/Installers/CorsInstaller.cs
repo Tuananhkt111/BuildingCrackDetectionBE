e9b4252db050384e2e1888f685eb6842e7f901fb
Tuan Anh <tuananhkt111@gmail.com>
2021-03-28 09:51:16 +0700

    update cors

--- CapstoneBE/Installers/CorsInstaller.cs
+++ CapstoneBE/Installers/CorsInstaller.cs
@@ -10,7 +10,7 @@ namespace CapstoneBE.Installers
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(builder =>{
-                    builder.WithOrigins("http://127.0.0.1:8080", "http://localhost:8080",
+                    builder.WithOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://192.168.1.4:8080",
                         "http://127.0.0.1:8082", "http://localhost:8082")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
